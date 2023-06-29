b1772c951604af3e53d1708cb8b56ba16385865c
Stephanie Santana <stephycsantana@gmail.com>
2023-06-29 12:42:45 -0700

    Basic login screen UI

--- InstagramClone/InstagramClone/ViewController.swift
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setUpUI()
+        
     }
 
-
+    func setUpUI() {
+        titleLabel.text = "Welcome to Instagram"
+        usernameLabel.text = "Username"
+        passwordLabel.text = "Password"
+        loginButton.setTitle("Login", for: .normal)
+        
+        titleLabel.textAlignment = .center
+        
+        loginButton.backgroundColor = .green
+        loginButton.setTitleColor(.white, for: .normal)
+    }
 }
 
